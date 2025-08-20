c1e882d5de8ed0bd4bdef519fc8e33acaa1753d8
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-08-20 22:14:56 +0200

    Increase default temp size to 256MB

--- extlib.h
+++ extlib.h
@@ -1741,7 +1741,7 @@ EXT_STATIC_ASSERT(((EXT_DEFAULT_ALIGNMENT) & ((EXT_DEFAULT_ALIGNMENT)-1)) == 0,
                   "default alignment must be a power of 2");
 
 #ifndef EXT_DEFAULT_TEMP_SIZE
-#define EXT_DEFAULT_TEMP_SIZE (8 * 1024 * 1024)
+#define EXT_DEFAULT_TEMP_SIZE (256 * 1024 * 1024)
 #endif
 
 static void *ext_default_alloc(Ext_Allocator *a, size_t size) {
